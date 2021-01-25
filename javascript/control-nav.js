15ddd498d5a8ece50d9e9de1efb26b09f9513970
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-01-25 12:27:59 +0700

    update 25 Jan 64

--- javascript/control-nav.js
+++
@@ -1,9 +0,0 @@
-function button_activated(item) {
-    var nav = document.getElementById(item);
-    if ( nav.style.display == "none" ) {
-        nav.style.display = "block";
-    }
-    else {
-        nav.style.display = "none";
-    } 
-}
